8e2930014596f14d3462e3e82eb2bcc391dc1f09
Stanislav <lemeshaevs@gmail.com>
2019-10-01 14:49:26 +0300

    Сделал CustomTableViewCell для ячейки

---
+++ MyPlaces/MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Станислав Лемешаев on 01/10/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
