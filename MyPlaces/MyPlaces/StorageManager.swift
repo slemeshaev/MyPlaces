d883cde2881d0d106714f5e8a970907cd2755359
Stanislav <lemeshaevs@gmail.com>
2019-10-03 23:43:28 +0300

    Created file StorageManager

---
+++ MyPlaces/MyPlaces/StorageManager.swift
@@ -0,0 +1,9 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Станислав Лемешаев on 03/10/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
