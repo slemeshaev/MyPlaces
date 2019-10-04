11f27240664e97a0c3aec2c91c103fe718e37915
Stanislav <lemeshaevs@gmail.com>
2019-10-04 22:39:30 +0300

    Delete Action

--- MyPlaces/MyPlaces/StorageManager.swift
+++ MyPlaces/MyPlaces/StorageManager.swift
@@ -17,6 +17,13 @@ class StorageManager {
         try! realm.write {
             realm.add(place)
         }
-        
     }
+    
+    static func deleteObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.delete(place)
+        }
+    }
+    
 }
