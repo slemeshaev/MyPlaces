a3fae109daf491e0fea67b353fc7031a11432c8f
Stanislav <lemeshaevs@gmail.com>
2019-10-04 16:44:41 +0300

    Refactor Code to Use DB

--- MyPlaces/MyPlaces/MainViewController.swift
+++ MyPlaces/MyPlaces/MainViewController.swift
@@ -7,21 +7,24 @@
 //
 
 import UIKit
+import RealmSwift
 
 class MainViewController: UITableViewController {
     
-    // var places = Place.getPlaces()
+    var places: Results<Place>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        places = realm.objects(Place.self)
     }
 
     // MARK: - Table view data source
 
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return places.count
-//    }
-    /*
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.isEmpty ? 0 : places.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
@@ -30,13 +33,7 @@ class MainViewController: UITableViewController {
         cell.nameLabel.text = place.name
         cell.locationLabel.text = place.location
         cell.typeLabel.text = place.type
-        
-        if place.image == nil {
-            cell.imageOfPlace.image = UIImage(named: place.restaurantImage!)
-        } else {
-            cell.imageOfPlace.image = place.image
-        }
-        
+        cell.imageOfPlace.image = UIImage(data: place.imageData!)
         
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
@@ -44,7 +41,6 @@ class MainViewController: UITableViewController {
         return cell
     }
     
-    */
     /*
     // MARK: - Navigation
 
@@ -60,7 +56,6 @@ class MainViewController: UITableViewController {
         guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
         
         newPlaceVC.saveNewPlace()
-        // places.append(newPlaceVC.newPlace!)
         tableView.reloadData()
     }
 
--- MyPlaces/MyPlaces/PlaceModel.swift
+++ MyPlaces/MyPlaces/PlaceModel.swift
@@ -15,28 +15,12 @@ class Place: Object {
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
     
-    let restaurantNames = [
-        "Burger Heroes", "Kitchen", "Bonsai",
-        "Балкан Гриль", "Вкусные истории",
-        "Пятница", "Классик", "Шок", "Бочка"
-    ]
-    
-    func savePlaces() {
-
-        for place in restaurantNames {
-            
-            let image = UIImage(named: place)
-            guard let imageData = image?.pngData() else { return }
-            
-            let newPlace = Place()
-            
-            newPlace.name = place
-            newPlace.location = "Ufa"
-            newPlace.type = "Restaurant"
-            newPlace.imageData = imageData
-            
-            StorageManager.saveObject(newPlace)
-            
-        }
+    convenience init(name: String, location: String?, type: String?, imageData: Data?) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
     }
+    
 }
