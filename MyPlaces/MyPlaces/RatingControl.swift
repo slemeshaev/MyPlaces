7532dc5c0f6a1e1b24478dd549a004782724d8dc
Stanislav <lemeshaevs@gmail.com>
2019-10-19 18:46:20 +0300

    Custom Rating Control: Part 2

--- MyPlaces/MyPlaces/RatingControl.swift
+++ MyPlaces/MyPlaces/RatingControl.swift
@@ -12,6 +12,12 @@ import UIKit
     
     // MARK: Properties
     
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionState()
+        }
+    }
+    
     private var ratingButtons = [UIButton]()
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
@@ -25,8 +31,6 @@ import UIKit
             setupButtons()
         }
     }
-    
-    var rating = 0
     
     // MARK: Initialization
     
@@ -43,7 +47,18 @@ import UIKit
     // MARK: Button Action
     
     @objc func ratingButtonTapped(button: UIButton) {
-        print("Button pressed 👍")
+        
+        guard let index = ratingButtons.firstIndex(of: button) else { return }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
+        }
+        
     }
     
     // MARK: Private Methods
@@ -57,10 +72,30 @@ import UIKit
         
         ratingButtons.removeAll()
         
+        // Load button image
+        let bundle = Bundle(for: type(of: self))
+        
+        let filledStar = UIImage(named: "blackStar",
+                                 in: bundle,
+                                 compatibleWith: self.traitCollection)
+        
+        let emptyStar = UIImage(named: "emptyStar",
+                                in: bundle,
+                                compatibleWith: self.traitCollection)
+        
+        let highlightedStar = UIImage(named: "highlightedStar",
+                                      in: bundle,
+                                      compatibleWith: self.traitCollection)
+        
         for _ in 0 ..< starCount {
             // Create the button
             let button = UIButton()
-            button.backgroundColor = .red
+            
+            // Set the button image
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -77,5 +112,13 @@ import UIKit
             ratingButtons.append(button)
         }
         
+        updateButtonSelectionState()
     }
+    
+    private func updateButtonSelectionState() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
+    }
+    
 }
