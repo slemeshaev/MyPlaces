602c16a2c55a9557785947f17f77cec0a58f113c
Stanislav <lemeshaevs@gmail.com>
2019-10-19 16:57:11 +0300

    Создал класс RatingControl

---
+++ MyPlaces/MyPlaces/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  MyPlaces
+//
+//  Created by Станислав Лемешаев on 19.10.2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
