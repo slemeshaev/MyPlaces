feac1b396a62724a367c45efaf045c095a0e8cc4
Stanislav <lemeshaevs@gmail.com>
2019-10-21 22:10:23 +0300

    Setup Placemark

--- MyPlaces/MyPlaces/MapViewController.swift
+++ MyPlaces/MyPlaces/MapViewController.swift
@@ -7,16 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var place: Place!
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupPlaceMark()
     }
 
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
     
+    private func setupPlaceMark() {
+        
+        guard let location = place.location else { return }
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(location) { (placemarks, error) in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let placemarks = placemarks else { return }
+            
+            let placemark = placemarks.first
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = self.place.name
+            annotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            annotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+        }
+    }
 }
