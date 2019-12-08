54e2e7f19b61dc4f18e6ca60da5d9c6bcdad97e4
Stanislav <lemeshaevs@gmail.com>
2019-12-08 23:16:12 +0300

    Создал файл cloud manager

---
+++ MyPlaces/MyPlaces/Helpers/CloudManager.swift
@@ -0,0 +1,9 @@
+//
+//  CloudManager.swift
+//  MyPlaces
+//
+//  Created by Станислав Лемешаев on 08.12.2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
